068f62327b93c14b42c5135bac4d3977bfb5b018
Michele Costa <michele.costa@unipart.io>
2020-10-07 18:15:26 +0100

    Split concept of dump and save and some extra type hinting
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- skeleton_etl/stores/abstract.py
+++ skeleton_etl/stores/abstract.py
@@ -1,33 +1,44 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, TypeVar, List, Iterator
+
+# Define some generic types
+T = TypeVar("T")
+K = TypeVar("K")
 
 
 class AbstractStore(ABC):
     @abstractmethod
-    def __len__(self):
+    def __len__(self) -> int:
         pass
 
     @staticmethod
-    def add(self, item: Any):
+    def add(self, value: T) -> K:
         """Returns key"""
         pass
 
     @abstractmethod
-    def get(self, key: Any):
+    def get(self, key: K) -> T:
         pass
 
     @abstractmethod
-    def remove(self, key: Any):
+    def remove(self, key: K) -> None:
         pass
 
     @abstractmethod
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         pass
 
     @abstractmethod
-    def load(self, **kwargs):
+    def save(self) -> None:
+        """Persist values"""
         pass
 
     @abstractmethod
-    def save(self, **kwargs):
+    def load(self, **kwargs) -> None:
+        """Load data from external persistance mechanism. Args will vary with mechanism"""
         pass
+
+    @abstractmethod
+    def dump(self) -> List[T]:
+        """Returns a list of the values stored to allow for values to be persisted elsewhere"""
+        return list(self)
