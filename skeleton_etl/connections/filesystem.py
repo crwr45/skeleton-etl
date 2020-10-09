58bb93ad0a5deb3e534f91f87ce6ccc50651473f
Michele Costa <michele.costa@unipart.io>
2020-10-09 10:01:07 +0100

    Add simple sanity check for message size for local connection
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- skeleton_etl/connections/filesystem.py
+++ skeleton_etl/connections/filesystem.py
@@ -4,6 +4,7 @@ import hashlib
 from ..models.message import Message
 
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
@@ -12,27 +13,34 @@ class LocalFilesystemConnection:
     def connect(_gateway):
         """Connect to local filesystem"""
         return pathlib.Path(pathlib.Path().absolute().root)
-    
-    @staticmethod
-    def receive_inputs(conn, path, message_selector):
+
+    @classmethod
+    def receive_inputs(cls, conn, path, message_selector):
 
         print(conn, path)
         directory = conn.joinpath(path)
 
         possible_files = directory.iterdir()
-    
+
         messages = []
         for filepath in possible_files:
             if not message_selector.message_selected(filepath):
                 continue
 
             data = filepath.read_bytes()
-            hash = hashlib.sha512(str(data).encode()).hexdigest()
-            messages.append(Message(
+            file_hash = hashlib.sha512(str(data).encode()).hexdigest()
+            message = Message(
                 name=filepath.name,
                 src_message_name=filepath.name,
-                src_message_hash=hash,
+                src_message_hash=file_hash,
                 data=data,
-            ))
-            # TODO: check file has been loaded completely. e.g. Size should match on disk.
+            )
+
+            if cls.check_message_consistency(message, filepath):
+                messages.append(message)
+
         return messages
+
+    @staticmethod
+    def check_message_consistency(message: Message, filepath: pathlib.Path) -> bool:
+        return len(message.data) == filepath.stat().st_size
